e9cb499a02137e3214704cce5e704295954a4095
ELY M. <elymbmx@gmail.com>
2026-09-21 15:44:50 -0500

    added few hosts

--- source/main.h
+++ source/main.h
@@ -47,6 +47,8 @@ const char *hostnames[] = {
     "nintendoswitch.com.cn",
     "nintendoswitch.cn",
     "sun.hac.lp1.d4c.nintendo.net",
+    "atumn.hac.lp1.d4c.nintendo.net",
+	"google.com"
 };
 
 #endif
